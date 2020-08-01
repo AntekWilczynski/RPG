66d38f49a5143e1f0420a299372780d327fecb9e
Antek Wilczyński <a.s.wilczynski@gmail.com>
2020-08-01 23:25:59 +0200

    first commit

---
+++ WPFUI/Engine/ViewModels/GameSesion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Zenon";
+            CurrentPlayer.CharacterClass = "Knight";
+            CurrentPlayer.Gold = 1000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Level = 1;
+
+
+
+
+
+        }
+
+
+    }
+}
